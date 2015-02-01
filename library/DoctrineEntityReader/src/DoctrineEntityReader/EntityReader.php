614c928e20871f18f2d86a7461b78f4bb0a4a162
stephan <stephan@debian>
2015-02-01 14:22:38 +0100

    reader classes added

---
+++ library/DoctrineEntityReader/src/DoctrineEntityReader/EntityReader.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace DoctrineEntityReader;
+
+use Doctrine\Common\Annotations\AnnotationReader;
+
+/**
+ * Creates profiles about entity Properties by reading the doctrine annotations
+ *
+ * @author   Cyberrebell <chainsaw75@web.de>
+ */
+class EntityReader
+{
+    /**
+     * Returns Properties determined by Entity-Namespace
+     * 
+     * @param string $entityNamespace Entity-Namespace
+     * @return array:\DoctrineEntityReader\Property
+     */
+    public static function getProperties($entityNamespace)
+    {
+        $reflectionClass = new \ReflectionClass($entityNamespace);
+        $reflectionProperties = $reflectionClass->getProperties();
+        
+        $properties = [];
+        foreach ($reflectionProperties as $reflectionProperty) {
+            $property = self::createProperty($reflectionProperty);
+            if ($property) {
+                $properties[$property->getName()] = $property;
+            }
+        }
+        return $properties;
+    }
+    
+    /**
+     * Returns created Property-Object
+     * gets Information from Reflection-Property which contains Doctrine-Annotations
+     * 
+     * @param \ReflectionProperty $reflectionProperty Reflection-Property of Entity
+     * 
+     * @throws \Exception
+     * @return boolean|\DoctrineEntityReader\Property
+     */
+    protected static function createProperty(\ReflectionProperty $reflectionProperty)
+    {
+        $property = new Property();
+        $property->setName($reflectionProperty->getName());
+        
+        $annotationReader = new AnnotationReader();
+        $annotations = $annotationReader->getPropertyAnnotations($reflectionProperty);
+        foreach ($annotations as $annotation) {
+            $annotationClassName = get_class($annotation);
+            if ($annotationClassName == 'Doctrine\ORM\Mapping\Id') {
+                return false;
+            } elseif ($annotationClassName == 'Doctrine\ORM\Mapping\Column') {
+                $property->setAnnotation($annotation);
+                $property->setType(Property::PROPERTY_TYPE_COLUMN);
+            } elseif ($annotationClassName == 'Doctrine\ORM\Mapping\ManyToOne'
+                || $annotationClassName == 'Doctrine\ORM\Mapping\OneToOne'
+            ) {
+                $property->setAnnotation($annotation);
+                $property->setType(Property::PROPERTY_TYPE_TOONE);
+                $property->setTargetEntity($annotation->targetEntity);
+            } elseif ($annotationClassName == 'Doctrine\ORM\Mapping\ManyToMany'
+                || $annotationClassName == 'Doctrine\ORM\Mapping\OneToMany'
+            ) {
+                $property->setAnnotation($annotation);
+                $property->setType(Property::PROPERTY_TYPE_TOMANY);
+                $property->setTargetEntity($annotation->targetEntity);
+            }
+        }
+        
+        if ($property->getType() == -1) {
+            throw new \Exception(
+                'Entity "' . $reflectionProperty->getDeclaringClass()->getName()
+                . '": defining annotation is missing at property "' . $property->getName() . '"!'
+            );
+        }
+        
+        return $property;
+    }
+}
---
+++ library/DoctrineEntityReader/src/DoctrineEntityReader/Property.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace DoctrineEntityReader;
+
+/**
+ * Container Class to store required information the EntityReader collected
+ * 
+ * @author   Cyberrebell <cyberrebell@web.de>
+ */
+class Property
+{
+    const PROPERTY_TYPE_COLUMN = 0;
+    const PROPERTY_TYPE_TOONE = 1;
+    const PROPERTY_TYPE_TOMANY = 2;
+    
+    protected $name;
+    protected $annotation;
+    protected $type = -1;
+    protected $targetEntity;
+    
+    /**
+     * Set the Name of Entity-Property
+     * 
+     * @param string $name Property-Name
+     * 
+     * @return null
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+    
+    /**
+     * Returns the Property-Name
+     * 
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+    
+    /**
+     * Set the annotation
+     * stored for extended information about property
+     *
+     * @param string $annotation Defining Annotation of Entity-Property
+     *
+     * @return null
+     */
+    public function setAnnotation($annotation)
+    {
+        $this->annotation = $annotation;
+    }
+    
+    /**
+     * Returns the annotation
+     * only for extended information about property
+     * 
+     * @return string
+     */
+    public function getAnnotation()
+    {
+        return $this->annotation;
+    }
+    
+    /**
+     * Set the type
+     * The constants of Property-Class are possible Types
+     *
+     * @param string $type Class-Constant
+     *
+     * @return null
+     */
+    public function setType($type)
+    {
+        $this->type = $type;
+    }
+    
+    /**
+     * Returns the Property-Type
+     * The constants of Property-Class are possible Types
+     *
+     * @return string
+     */
+    public function getType()
+    {
+        return $this->type;
+    }
+    
+    /**
+     * Set the target Entity-Namespace
+     * (is only set if Type is PROPERTY_TYPE_TOONE or PROPERTY_TYPE_TOMANY)
+     *
+     * @param string $targetEntity Entity-Namespace
+     *
+     * @return null
+     */
+    public function setTargetEntity($targetEntity)
+    {
+        $this->targetEntity = $targetEntity;
+    }
+    
+    /**
+     * Returns the target Entity-Namespace
+     * (is only set if Type is PROPERTY_TYPE_TOONE or PROPERTY_TYPE_TOMANY)
+     * 
+     * @return string
+     */
+    public function getTargetEntity()
+    {
+        return $this->targetEntity;
+    }
+}
